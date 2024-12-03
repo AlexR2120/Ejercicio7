3a51cddedf6e3d1369b80e007b2364708e6524d7
Alex Alumno Mac <alejandro_ruiz_m-tsapp1ma2324@cev.com>
2024-12-03 10:28:35 +0000

    Ejercicio 7

--- Ejercicio7/main.swift
+++ Ejercicio7/main.swift
@@ -1,11 +1,32 @@
-//
-//  main.swift
-//  Ejercicio7
-//
-//  Created by user231456 on 03/12/2024.
-//
+// EJERCICIO 7
 
-import Foundation
-
-print("Hello, World!")
+print("Introduce una frase: ")
+if let frase = readLine()
+{
+    // Separa las palabras de la frase
+    let palabras = frase.split(separator: " ").map { String($0) }
+    
+    // Crear Array para las palabras censuradas
+    var palabrasCensuradas = [String]()
+    
+    // Recorrer cada palabra
+    for palabra in palabras {
+        // Comprobar si empieza por "j", "p" o "m" (minúsculas o mayúsculas)
+        if palabra.lowercased().hasPrefix("j") || palabra.lowercased().hasPrefix("p")
+            || palabra.lowercased().hasPrefix("m") {
+            
+            // Añadirle asteríscos a las palabras censuradas
+            palabrasCensuradas.append("*****")
+        }
+        else {
+            // Si no, agregar la frase sin censura.
+            palabrasCensuradas.append(String(palabra))
+        }
+    }
+    
+    // Crear la frase
+    let resultado = palabrasCensuradas.joined(separator: " ")
+    
+    print(resultado)
+}
 
